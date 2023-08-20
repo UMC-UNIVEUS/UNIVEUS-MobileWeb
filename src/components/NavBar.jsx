fa78dbf54ec9989a994a37e30348cc90f29bc8e2
soi-ha <soy2302ten@gmail.com>
2023-08-20 19:55:31 +0900

    html 구현 완료2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,16 +8,16 @@ export default function NavBar() {
 	return (
 		<div className="nav-bar">
 			<div className="nb-home">
-				<img className="nb-btn-img" src="" alt="" />
-				<span className="nb-btn-text"></span>
+				<img className="nb-btn-img" src={Home} alt="홈 버튼 이미지" />
+				<span className="nb-btn-text">홈</span>
 			</div>
 			<div className="nb-search">
-				<img className="nb-btn-img" src="" alt="" />
-				<span className="nb-btn-text"></span>
+				<img className="nb-btn-img" src={Search} alt="검색 버튼 이미지" />
+				<span className="nb-btn-text">검색</span>
 			</div>
 			<div className="nb-mypage">
-				<img className="nb-btn-img" src="" alt="" />
-				<span className="nb-btn-text"></span>
+				<img className="nb-btn-img" src={MyPage} alt="마이페이지 버튼 이미지" />
+				<span className="nb-btn-text">마이페이지</span>
 			</div>
 		</div>
 	);
