bd8428b34a6c0223e84497baf490bf4ab9f3f8b1
jungwoo3490 <jungwoo3490@naver.com>
2023-09-06 16:33:47 +0900

    Logo 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,20 @@ import { useState } from 'react';
 
 import { ReactComponent as Bell } from '../assets/images/bell.svg';
 import { ReactComponent as BadgedBell } from '../assets/images/bell-badged.svg';
+import { ReactComponent as Logo } from '../assets/images/logo.svg';
 
 export const MainHeader = () => {
 
     const [isNewAlarm, setIsNewAlarm] = useState(false); // 새 알람 여부 State
-
+    
     return (
         <div className="MainHeader">
             <div className='header-left'>
-                <p className='univeus'>UNIVE.US</p>
-                <div className='graybar'></div>
-                <p className='kgu'>KGU</p>
+                <Logo />
+                <div className='headertext'>
+                    <p className='univeus'>UNIVE.US</p>
+                    <p className='kgu'>KGU</p>
+                </div>
             </div>
             <div className='header-right'>
                 {isNewAlarm === false ? 
