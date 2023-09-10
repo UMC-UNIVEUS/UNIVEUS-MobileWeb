752ae5d1eacd948cc989e9aad3c5656c38cd6327
soi-ha <soy2302ten@gmail.com>
2023-09-10 16:58:37 +0900

    Modal 내용 작성부분 수정

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ import './Modal.scss';
 // title={'타이틀로 넣을 글'}
 // content={'내용으로 넣을 글'}
 
-function Modal({ isOpen, closeModal, title, content }) {
+function Modal({ isOpen, closeModal, title, children }) {
 	return (
 		<div className="modal-parent">
 			<div className="modal-back" style={{ display: isOpen ? 'block' : 'none' }} onClick={closeModal}></div>
@@ -12,7 +12,7 @@ function Modal({ isOpen, closeModal, title, content }) {
 					<div className="mb-title" style={{ whiteSpace: 'pre-wrap' }}>
 						{title}
 					</div>
-					<div className="mb-content">{content}</div>
+					<div className="mb-content">{children}</div>
 				</div>
 			</div>
 		</div>
