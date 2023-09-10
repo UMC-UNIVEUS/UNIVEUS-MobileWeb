f363867c86ccb748a42e4bdf5f48c2b594c04d54
jungwoo3490 <jungwoo3490@naver.com>
2023-09-10 22:31:43 +0900

    PostPage API 연결

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -3,7 +3,7 @@ import { SubHeader } from "../components/Header";
 import '../pages/PostPage.scss'
 
 import DefaultProfileImg from '../assets/images/default_profile.svg';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GenderSticker } from "../components/MeetingCard";
 
 import MaleIcon from '../assets/images/male.svg';
@@ -11,6 +11,9 @@ import FemaleIcon from '../assets/images/female.svg';
 
 import test from '../assets/images/default_image.png'
 import Button from "../components/Button";
+import Modal from "../components/Modal";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 
 const PostPage = () => {
@@ -23,7 +26,101 @@ const PostPage = () => {
 
     const borderColor = gender === 1 ? '--purple-color' : '--pink-color';
 
-    const [postData, setPostData] = useState({});
+    const jwtToken = sessionStorage.getItem('accessToken');
+
+    const [postData, setPostData] = useState({
+        Post: {
+            "post_id": "",
+            "user_id": "",
+            "title": "",
+            "category": "",
+            "limit_gender": "",
+            "content": "",
+            "created_at": "",
+            "updated_at": "",
+            "scrapes": "",
+            "location": "",
+            "meeting_date": "",
+            "end_date": "",
+            "current_people": "",
+            "limit_people": "",
+            "main_img": "",
+            "openchat": "",
+            "post_status": "",
+            "likes": "",
+            "hidden": ""
+        },
+        PostImages: [],
+        Participant: []
+    });
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+  
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    const [invitee, setInvitee] = useState([]);
+	const [inviteeText, setInviteeText] = useState('');
+
+    const handleInvitee = (e) => {
+		setInvitee([...invitee, inviteeText]);
+	};
+
+    const handleInviteeText = (e) => {
+		setInviteeText(e.target.value);
+	};
+
+    const { id } = useParams();
+
+
+    useEffect(() => {
+        axios({
+            headers: {
+                "x-access-token": jwtToken
+            },
+            method: 'get',
+            url: `https://univeus.site/post/${id}`,
+        }).then((response) => {
+            setPostData(response.data.result);
+        })
+    }, []);
+
+    const limit_people = 6;
+
+    function repeatInvitee(limit_people) {
+		let arr = [];
+		for (let i = 0; i < limit_people / 2 - 1; i++) {
+			arr.push(
+				<input
+					type="text"
+					className="cfi-input"
+					id={i}
+					placeholder="친구의 닉네임을 입력해주세요"
+					required
+					onChange={handleInviteeText}
+					onBlur={handleInvitee}
+					key={i}
+				/>
+			);
+		}
+		return arr;
+	}
+
+    const handleClickInviteButton = () => {
+        axios({
+            headers: {
+                "x-access-token": jwtToken
+            },
+            method: 'post',
+            url: 'https://univeus.site/user/login',
+            data: {
+                
+            }
+        }).then((response) => {
+        
+        })
+    };
+
 
     return (
         <div className="PostPage">
@@ -54,8 +151,8 @@ const PostPage = () => {
                 </div>
                 <div className="horizontal_bar"></div>
                 <div className="postcontentcontainer">
-                    <p className="posttitle">제목을 입력해 주세요</p>
-                    <p className="postcontent">어떤 모임을 진행할 예정인가요? 간단하게 작성해 보아요 :) EX) 모임 주제 (바른 프로젝트 팀원을 구합니다.) 원하는 분위기나, 성격 원하는 과, 단과대 자신의 경험 사항(프로젝트, 공모전, 대외활동 등) 모임의 목표</p>
+                    <p className="posttitle">{postData.Post.title}</p>
+                    <p className="postcontent">{postData.Post.content}</p>
                 </div>
                 <div className="schedulecontainer">
                     <p className="scheduletitle">일정</p>
@@ -78,28 +175,35 @@ const PostPage = () => {
                         </div>
                         <div className="textbox">
                             <p>모임장소</p>
-                            <p className="meetingplace">5강 벤치앞</p>
+                            <p className="meetingplace">{postData.Post.location}</p>
                         </div>
                         <div className="textbox">
                             <p>참여인원</p>
-                            <p className="meetinglimit">2/<p style={{fontWeight: "var(--semi-bold)"}}>4</p></p>
+                            <p className="meetinglimit">{postData.Post.current_people}/<p style={{fontWeight: "var(--semi-bold)"}}>{postData.Post.limit_people}</p></p>
                         </div>
                     </div>
                 </div>
                 <div className="postpageimagecontainer">
-                    <PostImage imgsrc={test}/>
-                    <PostImage />
-                    <PostImage />
-                    <PostImage />
+                    {postData.PostImages.map((it) => (
+                        <PostImage imgsrc={it.img_url}/>
+                    ))}
                 </div>
                 <div className="horizontal_bar"></div>
                 <div className="participantscontainer">
                     <p className="participantcontainertitle">참여 친구</p>
                     <div className="participantlist">
-                        <ParticiPant participantGender={1}/>
+                        {postData.Participant.map((it, idx) => (
+                            <ParticiPant {...it} key={idx}/>
+                        ))} 
                     </div>
                 </div>
-                <Button type={'floating'} content={'참여하기'}/>
+                <Button type={'floating'} content={'참여하기'} onClick={openModal}/>
+                <Modal isOpen={isModalOpen} closeModal={closeModal} title={'함께 하는 친구를 초대해 주세요!'}>
+                    <div className="inviteelist">
+                        {repeatInvitee(limit_people)}
+                    </div>
+                    <Button content={"초대하기"} />
+                </Modal>
             </div>
         </div>
     );
@@ -113,22 +217,23 @@ export const PostImage = ({ imgsrc }) => {
     )
 };
 
-export const ParticiPant = ({participant_profile_img, participantGender}) => {
+export const ParticiPant = ({profile_img, gender, nickname, class_of}) => {
 
-    const participantBorderColor = participantGender === 1 ? '--purple-color' : '--pink-color';
+    const participantBorderColor = gender === 1 ? '--purple-color' : '--pink-color';
+    const classNum = class_of.substring(2, 4);
 
     return (
         <div className="Participant">
             <div className="flex-left">
                 <div className='participantprofileimage' style={{border: `2px solid var(${participantBorderColor})`}}>
-                    {participant_profile_img ? 
-                    <img src={participant_profile_img} alt="" className='userprofileimage'/> :
+                    {profile_img ? 
+                    <img src={profile_img} alt="" className='userprofileimage'/> :
                     <img src={DefaultProfileImg} alt="" className='defaultprofileimage'/>
                     }
                 </div>
                 <div className="participantinfo">
-                    <p className="participantnickname">테스트</p>
-                    <p className="participantclass">17학번</p>
+                    <p className="participantnickname">{nickname}</p>
+                    <p className="participantclass">{classNum}학번</p>
                     <p></p>
                 </div>
             </div>
