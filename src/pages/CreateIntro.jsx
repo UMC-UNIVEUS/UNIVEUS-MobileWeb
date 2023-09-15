79beba1456c8490bc040e07bd692122aec2c4aa2
soi-ha <soy2302ten@gmail.com>
2023-09-15 20:13:10 +0900

    이미지 빈배열 수정하기

--- src/pages/CreateIntro.jsx
+++ src/pages/CreateIntro.jsx
@@ -18,7 +18,9 @@ export default function CreateIntro() {
 	const { id } = useParams();
 
 	const getImage = (img) => {
-		setImage({ ...image, [Object.keys(JSON.parse(img))[0]]: Object.values(JSON.parse(img))[0] });
+		if (Object.values(JSON.parse(img))[0] !== '') {
+			setImage({ ...image, [Object.keys(JSON.parse(img))[0]]: Object.values(JSON.parse(img))[0] });
+		}
 	};
 
 	const handleTitle = (e) => {
@@ -30,9 +32,7 @@ export default function CreateIntro() {
 	};
 
 	const handleInvitee = (e) => {
-		if (inviteeText !== '') {
-			setInvitee({ ...invitee, [e.target.id]: inviteeText });
-		}
+		setInvitee({ ...invitee, [e.target.id]: inviteeText });
 	};
 
 	const handleInviteeText = (e) => {
@@ -157,16 +157,9 @@ export default function CreateIntro() {
 					{/* limitPeople의 갯수만큼 추가하기 */}
 					{repeatInvitee(createDetailData['limit_people'])}
 				</div>
-				{/* {title !== '' &&
-				content !== '' &&
-				Object.values(invitee).filter((item) => item !== '').length === createDetailData['limit_people'] / 2 - 1 ? (
-					<Button type={'floating'} content={'유니버스 생성하기'} onClick={handlePosting} />
-				) : (
-					<Button type={'floating disabled'} content={'미입력 된 항목이 있습니다'} />
-				)} */}
 				{title !== '' &&
 				content !== '' &&
-				Object.values(invitee).length === createDetailData['limit_people'] / 2 - 1 ? (
+				Object.values(invitee).filter((item) => item !== '').length === createDetailData['limit_people'] / 2 - 1 ? (
 					<Button type={'floating'} content={'유니버스 생성하기'} onClick={handlePosting} />
 				) : (
 					<Button type={'floating disabled'} content={'미입력 된 항목이 있습니다'} />
