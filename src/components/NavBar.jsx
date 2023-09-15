523c30b689bfb7cb318b58f5420f0553ab938684
soi-ha <soy2302ten@gmail.com>
2023-09-15 23:12:49 +0900

    nav 색상 수정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,33 @@
 import './NavBar.scss';
+import { useState, useEffect } from 'react';
 import { ReactComponent as Home } from '../assets/images/earth.svg';
 import { ReactComponent as Search } from '../assets/images/rocket.svg';
 import { ReactComponent as MyPage } from '../assets/images/person.svg';
 
 export default function NavBar(present) {
+	const [classHome, setClassHome] = useState('');
+	const [classSearch, setClassSearch] = useState('');
+	const [classMypage, setClassMypage] = useState('');
+	useEffect(() => {
+		if (Object.values(present)[0] === 'home') {
+			setClassHome('nb-home');
+		} else if (Object.values(present)[0] === 'search') {
+			setClassSearch('nb-search');
+		} else if (Object.values(present)[0] === 'mypage') {
+			setClassMypage('nb-mypage');
+		}
+	}, []);
 	return (
 		<div className="nav-bar">
-			<a className={present ? 'nb-link nb-home present' : 'nb-link nb-home'} href="/home">
+			<a className={`nb-link ${classHome}`} href="/home" style={{ marginRight: '96px' }}>
 				<Home className="nb-btn-img" />
 				<span className="nb-btn-text">홈</span>
 			</a>
-			<a className={present ? 'nb-link nb-search present' : 'nb-link nb-home'} href="/search">
+			<a className={`nb-link ${classSearch}`} href="/search" style={{ marginRight: '80px' }}>
 				<Search className="nb-btn-img" />
 				<span className="nb-btn-text">검색</span>
 			</a>
-			<a className={present ? 'nb-link nb-mypage present' : 'nb-link nb-home'} href="/myunive">
+			<a className={`nb-link ${classMypage}`} href="/myunive">
 				<MyPage className="nb-btn-img" />
 				<span className="nb-btn-text">마이페이지</span>
 			</a>
