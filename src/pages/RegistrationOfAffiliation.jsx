c52584e69d5884397a49ac8622938f7de073bf66
soi-ha <soy2302ten@gmail.com>
2024-01-15 21:30:48 +0900

    파일 대소문자 이름 변경 테스트

--- src/pages/RegistrationOfAffiliation.jsx
+++ src/pages/RegistrationOfAffiliation.jsx
@@ -3,10 +3,11 @@ import './RegistrationOfAffiliation.scss';
 import { SubHeader } from '../components/Header';
 import NavBar from '../components/NavBar';
 import Button from '../components/Button';
+
 export default function RegistrationOfAffiliation() {
 	<div className="registration-of-affiliation">
 		<SubHeader headertext={'소속등록'} />
-		<div className="roa-body">test</div>
+		<div className="roa-body">test테스트</div>
 		<NavBar />
 	</div>;
 }
