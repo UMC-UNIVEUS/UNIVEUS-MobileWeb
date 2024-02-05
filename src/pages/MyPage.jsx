bbb5777d14d4f2605546abfbda8bc0348a7a0fd6
soi-ha <soy2302ten@gmail.com>
2024-02-05 22:59:02 +0900

    API: 프로필 나의 정보 조회

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -8,15 +8,77 @@ import { ReactComponent as Setting } from '../assets/images/setting.svg';
 import { ReactComponent as Membership } from '../assets/images/membership.svg';
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function MyPage() {
 	const [cardList, setCardList] = useState([]);
-	const [userInfo, setUserInfo] = useState([]);
+	const [userInfo, setUserInfo] = useState({
+		nickname: '',
+		gender: '',
+		major: '',
+		student_id: '',
+		mebership: '',
+		user_img: '',
+		making: '',
+		participating: '',
+		introductionExist: '',
+	});
 	const [clicked, setClicked] = useState('create');
 	const navigate = useNavigate();
 
 	const handleMyUniv = () => {};
 	const handleParticipate = () => {};
+
+	// 채연 토큰
+	const jwtToken =
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEwLCJpYXQiOjE3MDU0NzE2MjMsImV4cCI6MTcxNDExMTYyMywiaXNzIjoidW5pdmV1cyJ9.FZ5uso5nr375V9N9IIT14KiKAW5GjPLZxWiFYsSdoAQ';
+
+	// 유저 정보 조회
+	const userInfoGet = async () => {
+		try {
+			const res = await axios.get('/profile/userInfo', {
+				headers: {
+					'x-access-token': jwtToken,
+				},
+			});
+			console.log(res);
+			setUserInfo(res.data.result.userInfo);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// 생성 정보 조회
+	const createInfoGet = async () => {
+		try {
+			const res = await axios.get('/profile/createInfo', {
+				headers: {
+					'x-access-token': jwtToken,
+				},
+			});
+			console.log(res);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// 참여 정보 조회
+	const participantInfoGet = async () => {
+		try {
+			const res = await axios.get('/profile/participant', {
+				headers: {
+					'x-access-token': jwtToken,
+				},
+			});
+			console.log(res);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		userInfoGet();
+	}, []);
 
 	return (
 		<div className="my-page">
@@ -24,10 +86,12 @@ export default function MyPage() {
 			<div className="mp-body">
 				<div className="mp-user">
 					<div className="mpu-top">
-						<Profile />
+						<Profile gender={userInfo.gender} profileImg={userInfo.user_img} />
 						<div className="mu-text">
-							<div className="mu-name">유니버스 관리자</div>
-							<div className="mu-classof">19학번 / 소프트웨어경영대학</div>
+							<div className="mu-name">{userInfo.nickname}</div>
+							<div className="mu-classof">
+								{userInfo.student_id} / {userInfo.major}
+							</div>
 						</div>
 						<Setting
 							className="mu-setting-img"
@@ -37,22 +101,32 @@ export default function MyPage() {
 						/>
 					</div>
 					{/* 멤버쉽 여부에 따라 display 조정 */}
-					<div className="mpu-middle">
-						<span className="mm-membership">BETA TESTER</span>
-						<Membership />
-					</div>
+					{userInfo.mebership === 'NO' ? (
+						''
+					) : (
+						<div className="mpu-middle">
+							<span className="mm-membership">BETA TESTER</span>
+							<Membership />
+						</div>
+					)}
+					{/* 작성 여부에 따라 문구 달라지기 */}
 					<div className="mpu-bottom">
 						<div className="mb-self-into-text">
-							{/* 작성 여부에 따라 문구 달라지기 <나의 N문 N답 보러가기> */}
-							<span className="mb-si-main">N문 N답으로 관심도를 높혀볼까요?</span>
-							{/* <추가 N문이 있는지 보러갈까요?> */}
-							<span className="mb-si-sub">유니버스 참여/생성 시 친구들이 볼 수 있어요 :)</span>
+							<span className="mb-si-main">
+								{userInfo.introductionExist ? '나의 N문 N답 보러가기' : 'N문 N답으로 관심도를 높혀볼까요?'}
+							</span>
+							<span className="mb-si-sub">
+								{userInfo.introductionExist
+									? '추가 N문이 있는지 보러갈까요?'
+									: '유니버스 참여/생성 시 친구들이 볼 수 있어요 :)'}
+							</span>
 						</div>
-						{/* 작성 여부에 따라 이동하는 페이지 변경 */}
 						<div
 							className="mb-self-into-btn"
 							onClick={() => {
-								navigate('/');
+								userInfo.introductionExist
+									? navigate('/profile/self-introduction')
+									: navigate('/profile/self-introduction-edit');
 							}}
 						>
 							CLICK!
