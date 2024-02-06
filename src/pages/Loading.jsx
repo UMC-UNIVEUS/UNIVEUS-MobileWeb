187bfb6fa1c8c35a71fc84ec824e1936d2d5be8d
soi-ha <soy2302ten@gmail.com>
2024-02-06 15:13:33 +0900

    chore: 로그인 인가코드 요청

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -4,64 +4,35 @@ import { useEffect, useState } from 'react';
 
 export default function Loading() {
 	const navigate = useNavigate();
-	const [accessToken, setAccessToken] = useState('');
+	const [accessCode, setAccessCode] = useState('');
 
-	console.log('accessToken1', accessToken);
+	console.log('accessCode1', accessCode);
 
 	useEffect(() => {
 		const parsedHash = new URLSearchParams(window.location.hash.substring(1));
-		const access_Token = parsedHash.get('access_token');
-		setAccessToken(access_Token);
-		console.log('accessToken2', accessToken);
+		console.log('hash', parsedHash);
+		const code = parsedHash.get('code');
+		setAccessCode(code);
+		console.log('code', code);
+		console.log('accessCode2', accessCode);
 
-		if (accessToken !== null && accessToken !== '') {
+		if (accessCode !== null && accessCode !== '') {
 			axios({
 				method: 'post',
 				url: '/user/login',
 				data: {
-					accessToken: accessToken,
+					code: accessCode,
 				},
 			})
 				.then((response) => {
 					console.log(response);
 					console.log('data', response.data);
 					console.log('data.code', response.data.code);
-					if (response.data.code === 1000) {
-						sessionStorage.setItem('accessToken', response.data.result.accessToken);
-						console.log('1000');
-						navigate('/home');
-					} else if (response.data.code === 2004) {
-						// 경기대 이메일 x
-						console.log('2004');
-						navigate('/');
-						// openModal5();
-					} else if (response.data.code === 'USER0011' || response.data.code === 'USER0012') {
-						// 본인인증 x -> 번호인증
-						// 번호인증 x -> 번호인증
-						sessionStorage.setItem('accessToken', response.data.result.accessToken);
-						console.log('번호인증');
-						navigate('/signup/identity');
-					} else if (response.data.code === 'USER0013') {
-						// 약관동의 x -> 약관동의
-						sessionStorage.setItem('accessToken', response.data.result.accessToken);
-						console.log('약관동의');
-						navigate('/signup/terms-of-use');
-					} else if (response.data.code === 'USER0014') {
-						// 소속인증 x -> 소속인증
-						sessionStorage.setItem('accessToken', response.data.result.accessToken);
-						console.log('소속인증');
-						navigate('/signup/registration-of-affiliation');
-					} else if (response.data.code === 'USER0015') {
-						// 프로필 등록 x -> 프로필 등록
-						sessionStorage.setItem('accessToken', response.data.result.accessToken);
-						console.log('프로필 등록');
-						navigate('/signup/register-profile');
-					}
 				})
 				.catch((error) => {
 					console.error('axios error:', error);
 				});
 		}
-	}, [accessToken]);
+	}, [accessCode]);
 	return <h1>Redirect Page</h1>;
 }
