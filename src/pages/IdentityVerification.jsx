d56c576c0c8dec9bd6de05ba0b3da6496af348b0
soi-ha <soy2302ten@gmail.com>
2024-02-07 22:53:05 +0900

    chore: redirect_uri 변경 및 token 전송 post 이동

--- src/pages/IdentityVerification.jsx
+++ src/pages/IdentityVerification.jsx
@@ -4,8 +4,11 @@ import NavBar from '../components/NavBar';
 import Button from '../components/Button';
 import CheckBtn from '../assets/images/arrow_circle.svg';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function IdentityVerification() {
+	const [accessToken, setAccessToken] = useState('');
+
 	const [phoneNumber, setPhoneNumber] = useState('');
 	const [verifyNumber, setVerifyNumber] = useState('');
 
@@ -21,6 +24,26 @@ export default function IdentityVerification() {
 	const handleVerifyNumber = (e) => {
 		setVerifyNumber(e.target.value.replace(/[^0-9]/g, ''));
 	};
+
+	// 서버에게 token 전송
+	const loginAxios = async () => {
+		try {
+			const res = await axios.post('/user/login', { token: accessToken });
+			console.log(res);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		const parsedHash = new URLSearchParams(window.location.hash.substring(1));
+		const token = parsedHash.get('access_token');
+		setAccessToken(token);
+
+		if (accessToken !== null && accessToken !== '') {
+			loginAxios();
+		}
+	}, [accessToken]);
 
 	return (
 		<div className="identity-verification">
