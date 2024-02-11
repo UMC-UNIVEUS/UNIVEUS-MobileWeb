a7202ad8ecd37459f90970293ace04590e2811d4
soi-ha <soy2302ten@gmail.com>
2024-02-12 02:02:56 +0900

    API: 프로필 등록 및 닉네임 중복

--- src/pages/RegisterProfile.jsx
+++ src/pages/RegisterProfile.jsx
@@ -3,14 +3,25 @@ import { SubHeader } from '../components/Header';
 import NavBar from '../components/NavBar';
 import Button from '../components/Button';
 import CheckBtn from '../assets/images/arrow_circle.svg';
+import DeleteBtn from '../assets/images/delete.svg';
 import Person from '../assets/images/person_fill.svg';
 import { ReactComponent as Pencil } from '../assets/images/write.svg';
+import Modal from '../components/Modal';
+import ContainsSpecialCharacter from '../utils/ContainsSpecialCharacter.jsx';
 import { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function RegisterProfile() {
+	const jwtToken = sessionStorage.getItem('accessToken');
+	const navigate = useNavigate();
+	const MAXSIZE = 1 * 1024 * 1024;
+	const [isNickname, setIsNickname] = useState('');
 	const [nickName, setNickName] = useState('');
-	const [gender, setGender] = useState(); // 1 or 2
-	const [profileImg, setProfileImg] = useState('');
+	const [gender, setGender] = useState('MAN'); // MAN, WOMAN
+	const [imgFile, setImgFile] = useState('');
+	const [errorImg, setErrorImg] = useState(false);
+	const [message, setMessage] = useState(''); // nicknameDuplicate, nicknameAvailable, nicknameUnavailable
 
 	const imgRef = useRef();
 
@@ -18,13 +29,87 @@ export default function RegisterProfile() {
 		setNickName(e.target.value);
 	};
 
-	const saveImgFile = () => {
+	const openErrorModal = () => {
+		setErrorImg(true);
+	};
+
+	const closeErrorModal = () => {
+		setErrorImg(false);
+	};
+
+	// 이미지 업로드 input의 onChange
+	const saveImgFile = (e) => {
 		const file = imgRef.current.files[0];
-		const reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onloadend = () => {
-			setProfileImg(reader.result);
-		};
+
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append('image', file);
+
+		if (file.size >= MAXSIZE) {
+			openErrorModal();
+		} else {
+			axios({
+				headers: {
+					'x-access-token': jwtToken,
+					'Content-Type': 'multipart/form-data',
+				},
+				method: 'post',
+				url: '/post/image/upload?directory=profile',
+				data: formData,
+			})
+				.then((res) => {
+					const updatedImgFile = [res.data.result[0]['pic_url']];
+					setImgFile(updatedImgFile);
+				})
+				.catch((err) => {
+					console.log('err : ', err);
+				});
+		}
+	};
+
+	// 닉네임 중복확인
+	const checkNickname = async () => {
+		const specialCharacter = ContainsSpecialCharacter(nickName);
+		if (nickName.length > 1 && !specialCharacter) {
+			const res = await axios.post(
+				'/user/nickname/check',
+				{
+					nickname: nickName,
+				},
+				{
+					headers: {
+						'x-access-token': jwtToken,
+					},
+				}
+			);
+			console.log(res);
+			if (res.data.code === 'COMMON200') {
+				setMessage('nicknameAvailable');
+				setIsNickname(nickName);
+			} else if (res.data.code === 2014) {
+				setMessage('nicknameDuplicate');
+			}
+		} else {
+			setMessage('nicknameUnavailable');
+		}
+	};
+
+	// 프로필 등록
+	const profilePost = async () => {
+		const res = await axios.post(
+			'/user/register/profile',
+			{
+				nickname: nickName,
+				gender: gender,
+			},
+			{
+				headers: {
+					'x-access-token': jwtToken,
+				},
+			}
+		);
+		console.log(res);
+		navigate('/home');
 	};
 
 	return (
@@ -35,16 +120,15 @@ export default function RegisterProfile() {
 					<label htmlFor="rpi-img-input">
 						<img
 							className="rpi-profile-img"
-							src={profileImg ? profileImg : Person}
+							src={imgFile ? imgFile : Person}
 							alt="프로필 이미지"
 							style={{
-								width: profileImg ? '' : '47px',
-								height: profileImg ? '' : '47px',
-								borderRadius: profileImg ? '' : '0px',
+								width: imgFile ? '' : '47px',
+								height: imgFile ? '' : '47px',
+								borderRadius: imgFile ? '' : '0px',
 							}}
 						/>
-						{profileImg ? <></> : <Pencil className="rpi-pencil-img" />}
-						{/* <Pencil className="rpi-pencil-img" /> */}
+						{imgFile ? <></> : <Pencil className="rpi-pencil-img" />}
 					</label>
 					<input
 						type="file"
@@ -54,6 +138,20 @@ export default function RegisterProfile() {
 						ref={imgRef}
 						style={{ display: 'none' }}
 					/>
+					<div className="rpi-delete">
+						{imgFile ? (
+							<img
+								className="rpi-delete-img"
+								src={DeleteBtn}
+								alt="이미지 삭제 버튼"
+								onClick={() => {
+									setImgFile('');
+								}}
+							/>
+						) : (
+							<></>
+						)}
+					</div>
 				</div>
 				<div className="rp-form">
 					<div className="rp-input-group">
@@ -65,8 +163,27 @@ export default function RegisterProfile() {
 							value={nickName}
 							className="rp-input"
 						/>
-						<img src={CheckBtn} alt="확인 버튼" className="rp-img" />
+						<img
+							src={CheckBtn}
+							alt="확인 버튼"
+							className="rp-img"
+							onClick={() => {
+								checkNickname();
+							}}
+						/>
 					</div>
+					<span
+						className="rp-message-text"
+						style={{ color: message === 'nicknameAvailable' ? 'var(--deep-blue-color)' : '' }}
+					>
+						{message === 'nicknameAvailable'
+							? '사용 가능한 닉네임입니다.'
+							: message === 'nicknameDuplicate'
+							? '중복된 닉네임입니다.'
+							: message === 'nicknameUnavailable'
+							? '올바르지 않은 형식입니다. (글자수 2~13자, 특수문자 미포함)'
+							: ''}
+					</span>
 					<div className="rp-radio-group">
 						<input
 							className="rp-radio"
@@ -75,7 +192,7 @@ export default function RegisterProfile() {
 							id="male"
 							defaultChecked
 							onClick={() => {
-								setGender(0);
+								setGender('MAN');
 							}}
 						/>
 						<label htmlFor="male">남성</label>
@@ -87,14 +204,30 @@ export default function RegisterProfile() {
 							name="gender"
 							id="female"
 							onClick={() => {
-								setGender(1);
+								setGender('WOMAN');
 							}}
 						/>
 						<label htmlFor="female">여성</label>
 					</div>
 				</div>
-				<Button type={'floating'} content={'완료'} />
+				{nickName !== '' && gender !== '' && nickName === isNickname ? (
+					<Button
+						type={'floating'}
+						content={'완료'}
+						onClick={() => {
+							profilePost();
+						}}
+					/>
+				) : (
+					<Button type={'floating disabled'} content={'완료'} />
+				)}
 			</div>
+			<Modal isOpen={errorImg} closeModal={closeErrorModal} title={'이미지 업로드 실패!'}>
+				<p style={{ color: 'rgba(0, 0, 0, 0.60)' }}>업로드가 불가능한 이미지입니다.</p>
+				<p style={{ color: 'rgba(0, 0, 0, 0.60)' }}>다른 이미지를 선택해주세요!</p>
+				<p style={{ color: 'rgba(241, 52, 52, 0.935)' }}>업로드 불가 사유: 업로드 불가 확장자 혹은 용량</p>
+				<Button type={'floating'} content={'확인'} onClick={closeErrorModal} />
+			</Modal>
 			<NavBar />
 		</div>
 	);
